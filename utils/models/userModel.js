b4eb8279fd487a82ed11a99ac77ec75072402a3b
Dhanush Saji <dhanushsaji1@gmail.com>
2023-12-24 01:14:18 +0530

    Initial commit from Create Next App

---
+++ utils/models/userModel.js
@@ -0,0 +1,9 @@
+import {mongoose} from "mongoose"
+
+const UserSchema = new mongoose.Schema({
+    name:{type:String},
+    email:{type:String,unique:true},
+    password:{type:String}
+},{timestamps:true})
+
+export const UserModel = mongoose.models.user || mongoose.model('user',UserSchema)
